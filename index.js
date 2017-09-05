60b583a27180ff74a10657d8c84d02b4b2c3181c
Olena Ageyeva <eageeva@hotmail.com>
2017-09-05 17:35:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const app = "I don't do much."
+kittens=["Milo", "Otis", "Garfield"]
+
+function appendKitten(name){
+  return(kittens...,name)
+}
+
+function destructivelyAppendKitten(name){
+  
+}
+
+function desructivelyPrependKitten(name){
+  
+}
+
+function destructivelyRemoveFirstKitten(){
+  
+}
+
+function destructivelyRemoveLastKitten(){
+  
+}
+
